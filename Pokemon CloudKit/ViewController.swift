ea785ac490cbf6367c7e398adfe6544c62e28d27
Eduardo Pereira <Dgp@Eduardos-MacBook-Pro.local>
2016-03-07 16:39:13 -0300

    first commit

---
+++ Pokemon CloudKit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pokemon CloudKit
+//
+//  Created by Eduardo Pereira on 3/7/16.
+//  Copyright © 2016 Eduardo Pereira. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
